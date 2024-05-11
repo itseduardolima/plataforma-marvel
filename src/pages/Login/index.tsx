03e4aaa5be1f3b8b9f90d3c10d97544f726303f3
itseduardolima <eduardolima2417@gmail.com>
2024-05-11 13:32:31 -0400

    update: components folder

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,7 @@
 import { FormWrapper, LoginContainer } from "./styled";
 import { LoginForm } from "../../components/LoginForm";
-import { Logo } from "../../shared/Logo";
-import { Background } from "../../shared/Background";
+import { Logo } from "../../components/Logo";
+import { Background } from "../../components/Background";
 
 export const Login = () => {
   return (
